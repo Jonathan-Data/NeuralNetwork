87c5ca2f831ae92d658e0370ff6a4bf5eaad6f28
wave <Jnthnlauwers@gmail.com>
2025-04-25 21:39:43 +0200

    removed comments

--- Neural-Network/neuron.py
+++ Neural-Network/neuron.py
@@ -1,4 +1,4 @@
-import numpy as np # type: ignore
+import numpy as np
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x));
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -12,15 +12,12 @@ def mse_loss(y_true, y_pred):
 
 class NeuralNetwork:
   def __init__(self):
-    # weights
     self.w1 = np.random.normal()
     self.w2 = np.random.normal()
     self.w3 = np.random.normal()
     self.w4 = np.random.normal()
     self.w5 = np.random.normal()
     self.w6 = np.random.normal()
-
-    # biases
     self.b1 = np.random.normal()
     self.b2 = np.random.normal()
     self.b3 = np.random.normal()
@@ -48,40 +45,26 @@ class NeuralNetwork:
         y_pred = o1
 
         d_L_d_ypred = -2 * (y_true - y_pred)
-
-        # Neuron o1
         d_ypred_d_w5 = h1 * deriv_sigmoid(sum_o1)
         d_ypred_d_w6 = h2 * deriv_sigmoid(sum_o1)
         d_ypred_d_b3 = deriv_sigmoid(sum_o1)
-
         d_ypred_d_h1 = self.w5 * deriv_sigmoid(sum_o1)
         d_ypred_d_h2 = self.w6 * deriv_sigmoid(sum_o1)
-
-        # Neuron h1
         d_h1_d_w1 = x[0] * deriv_sigmoid(sum_h1)
         d_h1_d_w2 = x[1] * deriv_sigmoid(sum_h1)
         d_h1_d_b1 = deriv_sigmoid(sum_h1)
-
-        # Neuron h2
         d_h2_d_w3 = x[0] * deriv_sigmoid(sum_h2)
         d_h2_d_w4 = x[1] * deriv_sigmoid(sum_h2)
         d_h2_d_b2 = deriv_sigmoid(sum_h2)
-
-        # Neuron h1
         self.w1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w1
         self.w2 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w2
         self.b1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_b1
-
-        # Neuron h2
         self.w3 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w3
         self.w4 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w4
         self.b2 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_b2
-
-        # Neuron o1
         self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_w5
         self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_w6
         self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_b3
-
       if epoch % 10 == 0:
         y_preds = np.apply_along_axis(self.feedforward, 1, data)
         loss = mse_loss(all_y_trues, y_preds)
